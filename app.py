5fed69465278120bfc1a3a5856e0c4273985b1d4
dolsen0 <derek.olsen@gmail.com>
2023-01-02 13:31:14 -0500

    translates from user prompt

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from translate import Translator
+
+#available languages:
+#(en, ja, ko, pt, zh, zh-TW, etc)
+
 translator = Translator(to_lang="pt")
 translation = translator.translate("very good, It's working")
 
-print(translation)
+message = input("\n What would you like to say? \n")
+conversation = translator.translate(message)
+
+print(conversation)
