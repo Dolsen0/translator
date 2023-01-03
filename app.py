0c8939cb7b72514011bf3622ef49eeaa146abd02
dolsen0 <derek.olsen@gmail.com>
2023-01-03 00:22:37 -0500

    improved readability of translation

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ translation = translator.translate("very good, It's working")
 message = input("\n What would you like to say? \n")
 conversation = translator.translate(message)
 
-speak(conversation, "pt", save=True, file="message.mp3", speak=True)
 
-
+#print(message)
 print(conversation)
+speak(message, "en", save=True, file="message_original.mp3", speak=True)
+speak(conversation, "pt", save=True, file="message_translated.mp3", speak=True)
+
