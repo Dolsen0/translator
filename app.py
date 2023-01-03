a08deb96d7d1576a8a023dd5a1dc6ff8be1e0d14
dolsen0 <derek.olsen@gmail.com>
2023-01-03 00:11:10 -0500

    added voice to speach functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from translate import Translator
+from text_to_speech import speak
 
 #available languages:
 #(en, ja, ko, pt, zh, zh-TW, etc)
@@ -8,5 +9,8 @@ translation = translator.translate("very good, It's working")
 
 message = input("\n What would you like to say? \n")
 conversation = translator.translate(message)
+
+speak(conversation, "pt", save=True, file="message.mp3", speak=True)
+
 
 print(conversation)
