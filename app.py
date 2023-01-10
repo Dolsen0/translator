4ab4c029c7ed1dd1826baddfc5e857b0cd25632e
dolsen0 <derek.olsen@gmail.com>
2023-01-09 23:18:57 -0500

    shortened code

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ from text_to_speech import speak
 #(en, ja, ko, pt, zh, zh-TW, etc)
 
 translator = Translator(to_lang="pt")
-translation = translator.translate("very good, It's working")
 
-message = input("\n What would you like to say? \n")
+message = input("\nWhat would you like to say? \n")
 conversation = translator.translate(message)
 
-
-#print(message)
 print(conversation)
 speak(message, "en", save=True, file="message_original.mp3", speak=True)
 speak(conversation, "pt", save=True, file="message_translated.mp3", speak=True)
